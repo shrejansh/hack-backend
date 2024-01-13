cd63afdab150c1c7b7dbddc4083d28d416949efd
Shreyansh <shreyanshanand54@gmail.com>
2024-01-13 17:02:28 +0530

    chore: Added cors

--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
 // import express from 'express';
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 import { checkExistingUser, createNewUser, getUsers } from './controller/users.controller';
 
 // const router = getRoutes();
 const app = express();
 const port = 4000 || process.env.PORT;
+app.use(cors())
 app.use(bodyParser.json());
-// app.use(function (req, res) {
-//     res.setHeader('Content-Type', 'text/plain')
-//     // res.write('you posted:\n')
-//     // res.end(JSON.stringify(req.body, null, 2))
-//   })
 app.get('/get-all', getUsers);
 app.post('/user', checkExistingUser);
 app.post('/create-user', createNewUser);
