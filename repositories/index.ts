033ca380b9b4f20b6f5f508bd88743849672f7f2
Shreyansh <shreyanshanand54@gmail.com>
2024-01-17 18:40:11 +0530

    chore: removed uri from codebase

--- repositories/index.ts
+++ repositories/index.ts
@@ -2,7 +2,7 @@ import {Sequelize, DataTypes} from 'sequelize';
 import pg from 'pg';
 import 'dotenv/config';
 
-const db_uri = process.env.DB_URI || 'postgres://hack_db_instance_user:YYwB3Ou4BE3gqXq4Y4CGxNuACrhRxEvn@dpg-cmguvmnqd2ns73fmtffg-a.singapore-postgres.render.com/hack_db_instance'
+const db_uri = process.env.DB_URI;
 const sequelize = new Sequelize(db_uri, {
   dialect: 'postgres',
   protocol: 'postgres',
